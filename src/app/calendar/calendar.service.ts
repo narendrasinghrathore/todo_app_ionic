31bfbb0016c8ccc7633c5e0c6cc97ef2226476e6
Narendra Singh Rathore <rathoreofficialemail@gmail.com>
2019-07-15 20:46:21 +0530

    Using ngZone to perform non angular task like loop.

--- src/app/calendar/calendar.service.ts
+++ src/app/calendar/calendar.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
 import { Observable, BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CalendarService {
+
+  constructor(private ngZone: NgZone) { }
 
   getTotalDaysOfMonth$: BehaviorSubject<CalendarMonth[]> = new BehaviorSubject([]);
 
@@ -18,18 +20,28 @@ export class CalendarService {
    */
   getTotalDaysOfMonth(year: number, month: number) {
     const lastDay = new Date(year, month, 0).getDate();
-    const arr = [...Array(lastDay).fill({
-      date: new Date(year, month - 1, 1),
-      day: 0
-    }).map((x: CalendarMonth, index) => {
-      const date = new Date(year, month - 1, index + 1);
-      const day = index + 1;
-      if (new Date().getDate() === date.getDate()) {
-        return { ...x, date, day, today: true };
-      }
-      return { ...x, date, day };
-    })];
-    this.getTotalDaysOfMonth$.next(arr);
+    /**
+     * Running functions via runOutsideAngular allows you to escape Angular's zone
+     * and do work that doesn't trigger Angular change-detection or is subject to
+     * Angular's error handling. Any future tasks or microtasks scheduled from
+     * within this function will continue executing from outside of the
+     * Angular zone. Use run to reenter the Angular zone
+     * and do work that updates the application model.
+     */
+    this.ngZone.runOutsideAngular(() => {
+      const arr = [...Array(lastDay).fill({
+        date: new Date(year, month - 1, 1),
+        day: 0
+      }).map((x: CalendarMonth, index) => {
+        const date = new Date(year, month - 1, index + 1);
+        const day = index + 1;
+        if (new Date().getDate() === date.getDate()) {
+          return { ...x, date, day, today: true };
+        }
+        return { ...x, date, day };
+      })];
+      this.getTotalDaysOfMonth$.next(arr);
+    });
   }
 
   /**
@@ -40,27 +52,37 @@ export class CalendarService {
   getWeekFromDay(date: Date) {
     const day = date.getDay();
     const array: CalendarWeek[] = [];
-    for (let i = 0; i < 7; i++) {
-      if (i - day !== 0) {
-        const days = i - day;
-        const newDate = new Date(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    /**
+     * Running functions via runOutsideAngular allows you to escape Angular's zone
+     * and do work that doesn't trigger Angular change-detection or is subject to
+     * Angular's error handling. Any future tasks or microtasks scheduled from
+     * within this function will continue executing from outside of the
+     * Angular zone. Use run to reenter the Angular zone
+     * and do work that updates the application model.
+     */
+    this.ngZone.runOutsideAngular(() => {
+      for (let i = 0; i < 7; i++) {
+        if (i - day !== 0) {
+          const days = i - day;
+          const newDate = new Date(date.getTime() + (days * 24 * 60 * 60 * 1000));
 
-        array.push({
-          date: newDate,
-          dayOfWeek: newDate.getDay(),
-          displayDate: newDate.toLocaleDateString(),
-          currentDate: false
-        });
-      } else {
-        array.push({
-          date,
-          displayDate: date.toLocaleDateString(),
-          dayOfWeek: date.getDay(),
-          currentDate: true
-        });
+          array.push({
+            date: newDate,
+            dayOfWeek: newDate.getDay(),
+            displayDate: newDate.toLocaleDateString(),
+            currentDate: false
+          });
+        } else {
+          array.push({
+            date,
+            displayDate: date.toLocaleDateString(),
+            dayOfWeek: date.getDay(),
+            currentDate: true
+          });
+        }
       }
-    }
-    this.getWeekFromDay$.next(array);
+      this.getWeekFromDay$.next(array);
+    });
   }
 }
 
