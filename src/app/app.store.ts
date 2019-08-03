875789cd39c3cc6b5c13e43b42bf589ecd282338
Narendra Singh Rathore <rathoreofficialemail@gmail.com>
2019-08-03 08:35:31 +0530

    Documentation

--- src/app/app.store.ts
+++ src/app/app.store.ts
@@ -7,19 +7,29 @@ import { Todo } from './models/todo.model';
 import { AppUser } from './models/User';
 import { AppUserData } from './models/firebase.model';
 
-
+/**
+ * Defining enum for type safety, it always a best practice.
+ * This enum are used for define the structure of state
+ */
 export enum AppStateProps {
     user = 'user',
     todos = 'todos',
     userProfileData = 'userProfileData',
     filteredTodos = 'filteredTodos'
 }
-
+/**
+ * Application state interface
+ */
 export interface State {
     [AppStateProps.user]: AppUser;
     [AppStateProps.todos]: Todo[];
     [AppStateProps.userProfileData]: AppUserData;
     [AppStateProps.filteredTodos]: Todo[];
+    /**
+     * Indexable types have an index signature that describes the types
+     * we can use to index into the object, along with the corresponding
+     * return types when indexing.
+     */
     [key: string]: any;
 }
 
@@ -29,7 +39,9 @@ const state: State = {
     [AppStateProps.userProfileData]: undefined,
     [AppStateProps.filteredTodos]: undefined
 };
-
+/**
+ * State management class having implementation to perform state changes
+ */
 export class Store {
     private subject = new BehaviorSubject<State>(state);
     private store = this.subject.asObservable()
